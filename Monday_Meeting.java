5e028e96b4d6aff06322109f8117d983fad056b1
Vladyslav Kostenko <41272025+vkostenk@users.noreply.github.com>
2022-08-29 14:30:36 -0700

    Add files via upload
    Files from 1st presentation

---
+++ Monday_Meeting.java
@@ -0,0 +1,77 @@
+Monday Group Meeting
+
+
+
+		- Constructor overloading 
+		 
+		Java is a technique that enables a single class to have more than
+		one constructor that varies by the list of arguments passed.
+		 Each overloaded constructor is used to perform different task in the class.
+
+
+		- Constructor vs method 
+
+		A Constructor is a block of code that initializes a newly created object.
+		 A Method is a collection of statements which returns a value upon its execution.
+		  A Constructor can be used to initialize an object.
+		   A Method consists of Java code to be executed.
+
+
+		   - Constructor chaining 
+
+		   Constructor chaining refers to the ability to call a constructor inside another constructor.
+		    You can use a constructor chain either within the same class or even with another one
+		     For the latter, the constructor should be through inheritance from the super class
+
+
+
+		    ``` Wrapper Classes ``` 
+
+		    - What is Wrapper classes ? 
+
+		    A Wrapper class is a class whose object wraps or contains primitive data types.
+		     When we create an object to a wrapper class, it contains a field and in this 
+		     field, we can store primitive data types.
+		      In other words, we can wrap a primitive value into a wrapper class object.
+
+
+		      - What autoboxing and unboxing 
+
+		      Autoboxing is the automatic conversion that the Java compiler makes between the primitive types and their corresponding object wrapper classes.
+		 	  For example, converting an int to an Integer, a double to a Double, and so on.
+		      If the conversion goes the other way, this is called unboxing.
+
+
+		      - Common Wrapper class methods we used 
+
+		      Primitive Type	Wrapper Class
+				byte					Byte
+				boolean					Boolean
+				char					Character
+				double					Double
+				float					Float
+				int						Integer
+				long					Long
+				short					Short
+
+
+				- What is ArrayList and how is it different from Arrays  
+
+				Array is a fixed length data structure whereas ArrayList is a variable length Collection class.
+				 We cannot change length of array once created in Java but ArrayList can be changed.
+				 We cannot store primitives in ArrayList, it can only store objects.
+				 But array can contain both primitives and objects in Java.
+
+
+
+				 - Can collections store primitives or objects  
+
+				 Java collections only store Objects, not primitive types; however we can store the wrapper classes
+
+
+
+
+
+
+
+
